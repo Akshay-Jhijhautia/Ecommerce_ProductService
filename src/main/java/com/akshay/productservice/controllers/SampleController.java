14baa25cc5c4543e6014dde051f0b87d83aeab3d
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2024-06-29 17:05:48 +0530

    added a basic api

---
+++ src/main/java/com/akshay/productservice/controllers/SampleController.java
@@ -0,0 +1,27 @@
+package com.akshay.productservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+// @Rest Controller means that this class will be hosting a set of HTTP API's
+@RestController
+@RequestMapping("/say")
+public class SampleController {
+
+    @GetMapping("/hello/{name}/{times}")
+    public String[] sayHello(@PathVariable("name") String name,
+                           @PathVariable("times") int times) {
+        String[] result = new String[times];
+        Arrays.fill(result, name);
+        return result;
+    }
+
+    @GetMapping("/cool")
+    public String cool(){
+        return "Welcome to Spring Boot";
+    }
+}
